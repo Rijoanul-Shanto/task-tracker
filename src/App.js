33b323fc5a56f813fbdc08750f660e6a55648a8f
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2021-02-22 01:46:55 +0600

    [ADD] - handle task delete implemented

--- src/App.js
+++ src/App.js
@@ -17,11 +17,15 @@ function App() {
       body: 'Task 3',
     },
   ]);
-  
+
+  const handleTaskDelete = (id) => {
+    console.log('delete...', id);
+  };
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={handleTaskDelete} />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,13 @@
-const Task = ({ task }) => {
-  return <h3 key={task.id}>{task.body}</h3>;
+import { FaTimes } from 'react-icons/fa';
+
+const Task = ({ task, onDelete }) => {
+  return (
+    <div className='task'>
+      <h3 key={task.id}>
+        {task.body} <FaTimes onClick={onDelete} color="red"/>
+      </h3>
+    </div>
+  );
 };
 
 export default Task;
